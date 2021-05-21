8ec9ff6ca0eece3bd1f108537c27d3b699ef170a
Jacquie Tran <jacquietran@users.noreply.github.com>
2021-05-22 09:12:50 +1000

    More Clifford attractors

--- chase_art/clifford_attractors_20210521.R
+++ chase_art/clifford_attractors_20210521.R
@@ -313,3 +313,179 @@ ggplot(df, aes(x, y)) + geom_point(color="#1E1E1E", shape=46, alpha=.01) + opt
 # Export plot to PNG
 ggsave(here("chase_art/everything/clifford_attractors_202105210130.png"),
        device = "png", width = 10, height = 8, units = "in", dpi = 600)
+
+# clifford_attractors_202105212147 ---------------------------------------------
+
+# Set plotting options
+#cream #FAF4E7
+#charcoal #1E1E1E
+opt = theme(legend.position  = "none",
+            panel.background = element_rect(fill="white"),
+            axis.ticks       = element_blank(),
+            panel.grid       = element_blank(),
+            axis.title       = element_blank(),
+            axis.text        = element_blank())
+
+# Define C++ function
+cppFunction('DataFrame createTrajectory(int n, double x0, double y0, 
+            double a, double b, double c, double d) {
+            // create the columns
+            NumericVector x(n);
+            NumericVector y(n);
+            x[0]=x0;
+            y[0]=y0;
+            for(int i = 1; i < n; ++i) {
+            x[i] = sin(a*y[i-1])+c*cos(a*x[i-1]);
+            y[i] = sin(b*x[i-1])+d*cos(b*y[i-1]);
+            }
+            // return a new data frame
+            return DataFrame::create(_["x"]= x, _["y"]= y);
+            }
+            ')
+
+a = 1.9
+b = 1.8
+c = -0.6
+d = -1
+
+# Create data frame
+df=createTrajectory(10000000, 0, 0, a, b, c, d)
+
+# Build plot
+ggplot(df, aes(x, y)) + geom_point(color="#1E1E1E", shape=46, alpha=.01) + opt
+
+# Export plot to PNG
+ggsave(here("chase_art/everything/clifford_attractors_202105212147.png"),
+       device = "png", width = 10, height = 8, units = "in", dpi = 600)
+
+# clifford_attractors_202105212158 ---------------------------------------------
+
+# Set plotting options
+#cream #FAF4E7
+#charcoal #1E1E1E
+opt = theme(legend.position  = "none",
+            panel.background = element_rect(fill="white"),
+            axis.ticks       = element_blank(),
+            panel.grid       = element_blank(),
+            axis.title       = element_blank(),
+            axis.text        = element_blank())
+
+# Define C++ function
+cppFunction('DataFrame createTrajectory(int n, double x0, double y0, 
+            double a, double b, double c, double d) {
+            // create the columns
+            NumericVector x(n);
+            NumericVector y(n);
+            x[0]=x0;
+            y[0]=y0;
+            for(int i = 1; i < n; ++i) {
+            x[i] = sin(a*y[i-1])+c*cos(a*x[i-1]);
+            y[i] = sin(b*x[i-1])+d*cos(b*y[i-1]);
+            }
+            // return a new data frame
+            return DataFrame::create(_["x"]= x, _["y"]= y);
+            }
+            ')
+
+a = 1.9
+b = 1.8
+c = -0.7
+d = -1
+
+# Create data frame
+df=createTrajectory(10000000, 0, 0, a, b, c, d)
+
+# Build plot
+ggplot(df, aes(x, y)) + geom_point(color="#1E1E1E", shape=46, alpha=.01) + opt
+
+# Export plot to PNG
+ggsave(here("chase_art/everything/clifford_attractors_202105212158.png"),
+       device = "png", width = 10, height = 8, units = "in", dpi = 600)
+
+# clifford_attractors_202105212200 ---------------------------------------------
+
+# Set plotting options
+#cream #FAF4E7
+#charcoal #1E1E1E
+opt = theme(legend.position  = "none",
+            panel.background = element_rect(fill="white"),
+            axis.ticks       = element_blank(),
+            panel.grid       = element_blank(),
+            axis.title       = element_blank(),
+            axis.text        = element_blank())
+
+# Define C++ function
+cppFunction('DataFrame createTrajectory(int n, double x0, double y0, 
+            double a, double b, double c, double d) {
+            // create the columns
+            NumericVector x(n);
+            NumericVector y(n);
+            x[0]=x0;
+            y[0]=y0;
+            for(int i = 1; i < n; ++i) {
+            x[i] = sin(a*y[i-1])+c*cos(a*x[i-1]);
+            y[i] = sin(b*x[i-1])+d*cos(b*y[i-1]);
+            }
+            // return a new data frame
+            return DataFrame::create(_["x"]= x, _["y"]= y);
+            }
+            ')
+
+a = 1.9
+b = 1.8
+c = -0.7
+d = -1.3
+
+# Create data frame
+df=createTrajectory(10000000, 0, 0, a, b, c, d)
+
+# Build plot
+ggplot(df, aes(x, y)) + geom_point(color="#1E1E1E", shape=46, alpha=.01) + opt
+
+# Export plot to PNG
+ggsave(here("chase_art/everything/clifford_attractors_202105212200.png"),
+       device = "png", width = 10, height = 8, units = "in", dpi = 600)
+
+# clifford_attractors_202105212202 ---------------------------------------------
+
+# Set plotting options
+#cream #FAF4E7
+#charcoal #1E1E1E
+opt = theme(legend.position  = "none",
+            panel.background = element_rect(fill="white"),
+            axis.ticks       = element_blank(),
+            panel.grid       = element_blank(),
+            axis.title       = element_blank(),
+            axis.text        = element_blank())
+
+# Define C++ function
+cppFunction('DataFrame createTrajectory(int n, double x0, double y0, 
+            double a, double b, double c, double d) {
+            // create the columns
+            NumericVector x(n);
+            NumericVector y(n);
+            x[0]=x0;
+            y[0]=y0;
+            for(int i = 1; i < n; ++i) {
+            x[i] = sin(a*y[i-1])+c*cos(a*x[i-1]);
+            y[i] = sin(b*x[i-1])+d*cos(b*y[i-1]);
+            }
+            // return a new data frame
+            return DataFrame::create(_["x"]= x, _["y"]= y);
+            }
+            ')
+
+a = 1.9
+b = 1.8
+c = -0.7
+d = 1.3
+
+# Create data frame
+df=createTrajectory(10000000, 0, 0, a, b, c, d)
+
+# Build plot
+ggplot(df, aes(x, y)) + geom_point(color="#1E1E1E", shape=46, alpha=.01) + opt
+
+# Export plot to PNG
+ggsave(here("chase_art/everything/clifford_attractors_202105212202.png"),
+       device = "png", width = 10, height = 8, units = "in", dpi = 600)
